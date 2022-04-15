d1d52e982448432573e0e1143977a7ce5413f6f3
RAMYANATH MAJHI <72814530+ramyanath@users.noreply.github.com>
2022-04-15 23:33:41 +0530

    updated with menu and sort function
    done

--- Suffle.js
+++ Suffle.js
@@ -1,14 +1,48 @@
+function bblSort(arr){
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+     
 function suffle(array){
-     array=array.sort((a,b)=> a-b);
-    let newArr=[],f1=0,f2=array.length/2;
-     for(let i = 0;i<array.length;i++){
-        if(i%2==0){
-            newArr[i]=array[f1++];
-        }else{
-            newArr[i]=array[f2++];
-        }
-     }
-    return newArr;
+ array=bblSort(array); 
+ let newArr=[],f1=0,f2=Math.trunc(array.length/2);
+  for(let i = 0;i<array.length;i++){
+    if (i % 2 == 0) {
+      newArr[i] = array[f1++];
+    } else {
+      newArr[i] = array[f2++];
+    }
+  }
+  window.alert(newArr);
 }
 
-console.log(suffle([2,1,5,3,11,7,45,76]))
+
+(()=>{
+    let arr=[];
+    while(1){
+        let opt=window.prompt(`
+        1 : Enter input
+        2 : Display 
+        3 : Exit
+        `);
+        opt=parseInt(opt)
+
+        switch (opt) {
+          case 1:
+            arr.push(parseInt(window.prompt(`Enter number : `)));
+            break;
+          case 2: suffle(arr);
+            break;
+          case 3:
+            return;
+        }
+    }
+})()
